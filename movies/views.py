2720e1a116a1584e167fa0be90b1732a5c69040f
massadd <rybyakov.kirill@gmail.com>
2020-04-06 19:28:12 +0300

    Initial commit

---
+++ movies/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+from .models import Movie
+
+
+class MoviesView(View):
+    """Список фильмов"""
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movies.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    # Описание фильма
+
+    def get(self, pk):
+        movie = Movie.objects.get(id=pk)
+        return render(request, "movies/movie_detail.html", {"movie": movie})
